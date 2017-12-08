c066a42828565bbaab16fee3462169c13582088a
andres <andresxv@gmail.com>
2017-12-08 17:50:38 -0500

    wp & _s installed

---
+++ wp-content/themes/marys-crew-house/inc/jetpack.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Jetpack Compatibility File
+ *
+ * @link https://jetpack.com/
+ *
+ * @package Marys_Crew_House
+ */
+
+/**
+ * Jetpack setup function.
+ *
+ * See: https://jetpack.com/support/infinite-scroll/
+ * See: https://jetpack.com/support/responsive-videos/
+ * See: https://jetpack.com/support/content-options/
+ */
+function marys_crew_house_jetpack_setup() {
+	// Add theme support for Infinite Scroll.
+	add_theme_support( 'infinite-scroll', array(
+		'container' => 'main',
+		'render'    => 'marys_crew_house_infinite_scroll_render',
+		'footer'    => 'page',
+	) );
+
+	// Add theme support for Responsive Videos.
+	add_theme_support( 'jetpack-responsive-videos' );
+
+	// Add theme support for Content Options.
+	add_theme_support( 'jetpack-content-options', array(
+		'post-details' => array(
+			'stylesheet' => 'marys-crew-house-style',
+			'date'       => '.posted-on',
+			'categories' => '.cat-links',
+			'tags'       => '.tags-links',
+			'author'     => '.byline',
+			'comment'    => '.comments-link',
+		),
+	) );
+}
+add_action( 'after_setup_theme', 'marys_crew_house_jetpack_setup' );
+
+/**
+ * Custom render function for Infinite Scroll.
+ */
+function marys_crew_house_infinite_scroll_render() {
+	while ( have_posts() ) {
+		the_post();
+		if ( is_search() ) :
+			get_template_part( 'template-parts/content', 'search' );
+		else :
+			get_template_part( 'template-parts/content', get_post_format() );
+		endif;
+	}
+}
---
+++ wp-content/themes/marys-crew-house/template-home.php
@@ -0,0 +1,11 @@
+
+<?php
+/**
+ * Template Name: Home
+ */
+
+get_header(); ?>
+
+	HOME
+<?php
+get_footer();
